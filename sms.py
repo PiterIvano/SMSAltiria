9695831ae5828823cc9b834396c1eae61a518524
Hidden-parker <68443246+Hidden-parker@users.noreply.github.com>
2021-02-20 01:02:30 -0600

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,12 +1,31 @@
 import clx.xms
 import requests
-print("-----------------------------")
-print("     Creado por Piter        ")
-print("-----------------------------")
+print("----------------------------------------------------------")
+print("-----------------Creado por Piter-------------------------")
+print("----------------------------------------------------------")
+print("----------------------------------------------------------")
+print("------------------Sk es puto------------------------------")
+print("----------------------------------------------------------")
 
-input("escriba su numero con la entrada de pais sin el + ->")
+
+en = input("escriba su numero con la entrada de pais sin el + -> ")
 print("----------------------------------------------------------")
-input("Escriba el texto a enviar ->")
+te = input("Escriba el texto a enviar -> ")
 print("----------------------------------------------------------")
+client = clx.xms.Client(service_plan_id='a60883dd6bdb49769705c63087633849', token='5702bea1c99c4dd29e7b46a9a9892ea4')
+
+
+create = clx.xms.api.MtBatchTextSmsCreate()
+create.sender = '447537404817'
+create.recipients = {en}
+create.body = te
+
+try:
+  batch = client.create_batch(create)
+except (requests.exceptions.RequestException,
+  clx.xms.exceptions.ApiException) as ex:
+  print('Failed to communicate with XMS: %s' % str(ex))
 
 print ("Su sms se a enviado")
+print("----------------------------------------------------------")
+print("----------------Chicos este es mi ultimo escript----------")
